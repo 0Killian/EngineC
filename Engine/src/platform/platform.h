8002339539e9064315853bb93f30095e77e223c0
Killian <killianbellouard@gmail.com>
2024-06-11 16:18:11 +0200

    Added console writing functions in platform

--- Engine/src/platform/platform.h
+++ Engine/src/platform/platform.h
@@ -3,13 +3,33 @@
  * @author Killian Bellouard (killianbellouard@gmail.com)
  * @brief This file defines the interface of the platform layer. Each platform must implement this interface in a separate .c
  * file.
- * @version 0.1
+ * @version 0.2
  * @date 2024-06-11
  */
 
 #pragma once
 
 #include "common.h"
+
+/** @brief The console colors used by the platform layer. */
+typedef enum platform_console_color {
+    PLATFORM_CONSOLE_COLOR_BLACK = 0,
+    PLATFORM_CONSOLE_COLOR_BLUE,
+    PLATFORM_CONSOLE_COLOR_GREEN,
+    PLATFORM_CONSOLE_COLOR_CYAN,
+    PLATFORM_CONSOLE_COLOR_RED,
+    PLATFORM_CONSOLE_COLOR_PURPLE,
+    PLATFORM_CONSOLE_COLOR_YELLOW,
+    PLATFORM_CONSOLE_COLOR_WHITE,
+    PLATFORM_CONSOLE_COLOR_GRAY,
+    PLATFORM_CONSOLE_COLOR_LIGHT_BLUE,
+    PLATFORM_CONSOLE_COLOR_LIGHT_GREEN,
+    PLATFORM_CONSOLE_COLOR_LIGHT_CYAN,
+    PLATFORM_CONSOLE_COLOR_LIGHT_RED,
+    PLATFORM_CONSOLE_COLOR_LIGHT_PURPLE,
+    PLATFORM_CONSOLE_COLOR_LIGHT_YELLOW,
+    PLATFORM_CONSOLE_COLOR_BRIGHT_WHITE
+} platform_console_color;
 
 /**
  * @brief Initializes the platform layer.
@@ -22,11 +42,31 @@
  * @retval TRUE Success
  * @retval FALSE Failure
  */
-API b8 platform_init(void* state, u64* size_requirement);
+b8 platform_init(void* state, u64* size_requirement);
 
 /**
  * @brief Deinitializes the platform layer.
  * 
  * @param[in] state A pointer to the state of the platform layer.
  */
-API void platform_deinit(void* state);
+void platform_deinit(void* state);
+
+/**
+ * @brief Writes a message to the console.
+ * 
+ * Message considered as errors should use @ref platform_console_write_error instead
+ * 
+ * @param[in] foreground The color of the text
+ * @param[in] background The color of the background
+ * @param[in] message The message to write
+ */
+void platform_console_write(platform_console_color foreground, platform_console_color background, const char* message);
+
+/**
+ * @brief Writes an error message to the console.
+ * 
+ * @param[in] foreground The color of the text
+ * @param[in] background The color of the background
+ * @param[in] message The message to write
+ */
+void platform_console_write_error(platform_console_color foreground, platform_console_color background, const char* message);
--- Engine/src/platform/platform_win32.c
+++ Engine/src/platform/platform_win32.c
@@ -2,11 +2,18 @@
 
 #if PLATFORM_WINDOWS
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 /** @brief State of the platform layer. */
 typedef struct platform_state {    
 } platform_state;
 
 static platform_state *state;
+
+static u8 convert_platform_color(platform_console_color foreground, platform_console_color background) {
+    return foreground & 0xF | (background & 0xF) << 4;
+}
 
 /**
  * @brief Initializes the platform layer.
@@ -38,6 +45,42 @@ b8 platform_init(void* state_storage, u64* size_requirement) {
  */
 void platform_deinit(void* state) {
     // There is nothing to do here...
+}
+
+static console_write(u8 color, const char* message, HANDLE console_handle) {
+    CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+    GetConsoleScreenBufferInfo(console_handle, &buffer_info);
+
+    SetConsoleTextAttribute(console_handle, color);
+    OutputDebugStringA(message);
+    DWORD written = 0;
+    WriteConsoleA(console_handle, message, (DWORD)strlen(message), &written, NULL);
+
+    SetConsoleTextAttribute(console_handle, buffer_info.wAttributes);
+}
+
+/**
+ * @brief Writes a message to the console.
+ * 
+ * Message considered as errors should use @ref platform_console_write_error instead
+ * 
+ * @param[in] foreground The color of the text
+ * @param[in] background The color of the background
+ * @param[in] message The message to write
+ */
+void platform_console_write(platform_console_color foreground, platform_console_color background, const char* message) {
+    console_write(convert_platform_color(foreground, background), message, GetStdHandle(STD_OUTPUT_HANDLE));
+}
+
+/**
+ * @brief Writes an error message to the console.
+ * 
+ * @param[in] foreground The color of the text
+ * @param[in] background The color of the background
+ * @param[in] message The message to write
+ */
+void platform_console_write_error(platform_console_color foreground, platform_console_color background, const char* message) {
+    console_write(convert_platform_color(foreground, background), message, GetStdHandle(STD_ERROR_HANDLE));
 }
 
 #endif
