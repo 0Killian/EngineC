21f2bf51ab81fab41aec0a237f8801ac617d9cbb
Killian <killianbellouard@gmail.com>
2024-07-19 20:54:22 +0200

    Vec2f structure

---
+++ Engine/src/math/vec2.h
@@ -0,0 +1,16 @@
+/**
+ * @file event.h
+ * @author Killian Bellouard (killianbellouard@gmail.com)
+ * @brief This file defines 2-component vectors and functions for them.
+ * @version 0.1
+ * @date 2024-07-19
+ */
+
+#pragma once
+
+#include "common.h"
+
+typedef struct vec2f {
+    f32 x;
+    f32 y;
+} vec2f;
